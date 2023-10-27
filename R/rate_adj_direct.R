e06786afed03d770b1be4071f2ed59addafa329e
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-27 16:30:51 +0200

    argument names standardization progress bar

--- R/rate_adj_direct.R
+++ R/rate_adj_direct.R
@@ -5,10 +5,11 @@
 #' @param .data A tibble containing events counts and population per groups (e.g. age groups)
 #' @param .std A vector with standard population values for each group
 #' @param .keys Optional. A character vector with grouping variables, like year and region code.
-#' @param name_var Variable containing variable names. Defaults to `name`.
-#' @param value_var Variable containing values. Defaults to `value`.
-#' @param events_label Label used for events at the `name_var` variable. Defaults to `events`.
-#' @param population_label Label used for population at the `name_var` variable. Defautls to `population`.
+#' @param .name_var Variable containing variable names. Defaults to `name`.
+#' @param .value_var Variable containing values. Defaults to `value`.
+#' @param .events_label Label used for events at the `name_var` variable. Defaults to `events`.
+#' @param .population_label Label used for population at the `name_var` variable. Defautls to `population`.
+#' @param .progress Whether to show a progress bar. Defaults to `TRUE`.
 #'
 #' @return A tibble with crude and adjusted rate, lower and upper confidence intervals.
 #' @export
@@ -17,8 +18,9 @@
 #' standard <- c(63986.6, 186263.6, 157302.2, 97647.0, 47572.6, 12262.6)
 #' rate_adj_direct(fleiss_data, .std = standard)
 rate_adj_direct <- function(.data, .std, .keys = NULL,
-                            name_var = "name", value_var = "value",
-                            events_label = "events", population_label = "population"){
+                            .name_var = "name", .value_var = "value",
+                            .events_label = "events", .population_label = "population",
+                            .progress = TRUE){
 
   # Group by keys, if available
   if(!is.null(.keys)){
@@ -28,7 +30,7 @@ rate_adj_direct <- function(.data, .std, .keys = NULL,
 
   # Pivot variables and split to list
   res_list <- .data %>%
-    tidyr::pivot_wider(names_from = name_var, values_from = value_var) %>%
+    tidyr::pivot_wider(names_from = .name_var, values_from = .value_var) %>%
     dplyr::group_split(.keep = TRUE)
 
   # Map
@@ -36,12 +38,12 @@ rate_adj_direct <- function(.data, .std, .keys = NULL,
     tibble::as_tibble(
       t(
         epitools::ageadjust.direct(
-          count = get({events_label}, x),
-          pop = get({population_label}, x),
+          count = get({.events_label}, x),
+          pop = get({.population_label}, x),
           stdpop = std
         )
       ))
-  }) %>%
+  }, .progress = .progress) %>%
     purrr::list_rbind()
 
   # Add keys to result, if available
