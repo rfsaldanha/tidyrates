830bfd9cccb02ca04ad225e8fcf737f0dbec296f
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-27 17:22:33 +0200

    simplify test

--- tests/testthat/test-rate_adj_direct.R
+++ tests/testthat/test-rate_adj_direct.R
@@ -1,7 +1,7 @@
 test_that("rate_adj_direct works", {
 
   standard <- c(63986.6, 186263.6, 157302.2, 97647.0, 47572.6, 12262.6)
-  res <- rate_adj_direct(fleiss_data, .std = standard, .keys = "key", .name_var = "name")
+  res <- rate_adj_direct(fleiss_data, .std = standard, .keys = "key")
 
   expect_equal(nrow(res), 5)
 })
