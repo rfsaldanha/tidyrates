8f938545223df9df5f68e6aefb34cb41ed26f093
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-30 12:53:01 +0100

    Update documentation Assert age groups are equal Correct test and example typo

--- R/rate_adj_direct.R
+++ R/rate_adj_direct.R
@@ -4,6 +4,10 @@
 #'
 #' This functions wraps the `epitools` \link[epitools]{ageadjust.direct} function to compute direct adjusted rates and "exact" confidence intervals using `tibble` objects with multiple grouping keys.
 #'
+#' A tibble (`.data`) must be informed containing key variables like year and region code, and population and and events count (e.g. cases) per age group. Check the `fleiss_data` for an example.
+#'
+#' A tibble (`.std`) must be also supplied containing the age groups and population size. By default, this tibble has two variables, named `age_group` and `pop`.
+#'
 #' @param .data A tibble containing events counts and population per groups (e.g. age groups)
 #' @param .std A vector with standard population values for each group
 #' @param .keys Optional. A character vector with grouping variables, like year and region code.
@@ -21,7 +25,10 @@
 #' @importFrom rlang :=
 #'
 #' @examples
-#' standard_pop <- c(63986.6, 186263.6, 157302.2, 97647.0, 47572.6, 12262.6)
+#' standard_pop <- tibble::tibble(
+#'    age_group = c("Under 20", "20-24", "25-29", "30-34", "35-39", "40 and over"),
+#'    pop = c(63986.6, 186263.6, 157302.2, 97647.0, 47572.6, 12262.6)
+#'  )
 #' rate_adj_direct(fleiss_data, .std = standard_pop)
 rate_adj_direct <- function(.data,
                             .std,
@@ -54,6 +61,7 @@ rate_adj_direct <- function(.data,
   checkmate::assert_choice(.age_group_pop_var, names(.std))
   checkmate::assert_choice(.events_label, unique(get({.name_var}, .data)))
   checkmate::assert_choice(.population_label, unique(get({.name_var}, .data)))
+  checkmate::assert_set_equal(sort(unique(get({.age_group_var}, .data))), sort(unique(get({.age_group_var}, .std))))
 
   # Group by keys, if available
   if(!is.null(.keys)){
