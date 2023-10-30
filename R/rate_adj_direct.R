e148cc4816743cb92bba3af26f1d02c26655c348
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-30 12:06:28 +0100

    Flexible age group and population variables names.

--- R/rate_adj_direct.R
+++ R/rate_adj_direct.R
@@ -16,11 +16,15 @@
 #' @return A tibble with crude and adjusted rate, lower and upper confidence intervals.
 #' @export
 #'
+#' @importFrom rlang :=
+#'
 #' @examples
 #' standard_pop <- c(63986.6, 186263.6, 157302.2, 97647.0, 47572.6, 12262.6)
 #' rate_adj_direct(fleiss_data, .std = standard_pop)
 rate_adj_direct <- function(.data, .std, .keys = NULL,
                             .name_var = "name", .value_var = "value",
+                            .age_group_var = "age_group",
+                            .age_group_pop_var = "pop",
                             .events_label = "events", .population_label = "population",
                             .progress = TRUE){
 
@@ -32,7 +36,10 @@ rate_adj_direct <- function(.data, .std, .keys = NULL,
 
   # Assure age group factor order
   .data <- .data %>%
-    dplyr::mutate(age_group = forcats::fct_relevel(age_group, sort))
+    dplyr::mutate(!!dplyr::sym(.age_group_var) := forcats::fct_relevel(!!dplyr::sym(.age_group_var), sort))
+
+  .std <- .std %>%
+    dplyr::mutate(!!dplyr::sym(.age_group_var) := forcats::fct_relevel(!!dplyr::sym(.age_group_var), sort))
 
   # Pivot variables and split to list
   res_list <- .data %>%
@@ -40,7 +47,7 @@ rate_adj_direct <- function(.data, .std, .keys = NULL,
     dplyr::group_split(.keep = TRUE)
 
   # Map
-  res <- purrr::map(.x = res_list, .f = function(x, std = .std){
+  res <- purrr::map(.x = res_list, .f = function(x, std = get({.age_group_pop_var}, .std)){
     tibble::as_tibble(
       t(
         epitools::ageadjust.direct(
