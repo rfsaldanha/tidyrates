d322d2be0029d18edc564ac5db8ce33562cd2e91
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-30 16:41:24 +0100

    Update documentation.

--- R/fleiss_data.R
+++ R/fleiss_data.R
@@ -1,5 +1,5 @@
 #' Fleiss data
 #'
-#' Fleiss dataset from `epitools` package examples, with counts and population per age group in tidy format.
+#' Fleiss dataset from `epitools` package examples, with event counts and population per age group in tidy format.
 #'
 "fleiss_data"
