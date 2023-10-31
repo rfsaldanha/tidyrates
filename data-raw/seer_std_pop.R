c1e72cb1e8b9f8d6bb3a40a916fa980db800e355
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-31 15:09:25 +0100

    update population data

--- data-raw/seer_std_pop.R
+++ data-raw/seer_std_pop.R
@@ -1,6 +1,6 @@
 ## code to prepare `seer_std_pop` dataset goes here
 
 seer_std_pop <- readr::read_csv2(file = "data-raw/std_pop.csv") |>
-  dplyr::select(age_group, pop = seer_std)
+  dplyr::select(age_group, population = seer_std)
 
 usethis::use_data(seer_std_pop, overwrite = TRUE)
